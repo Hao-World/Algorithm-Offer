f36c7724eb2a44ba08397be91dece1d9d2864b9d
iiHao <hg1111116@163.com>
2019-08-05 02:06:27 +0800

    offer009

--- src/com/hao/offer/Offer009.java
+++ src/com/hao/offer/Offer009.java
@@ -22,16 +22,14 @@ public class Offer009 {
 
 	public int pop() {
 		if (stack2.isEmpty()) {
-			while (!stack1.isEmpty()) {
-				stack2.push(stack1.pop());
+			if (stack1.isEmpty()) {
+				throw new IllegalStateException("队列为空");
+			} else {
+				while (!stack1.isEmpty()) {
+					stack2.push(stack1.pop());
+				}
 			}
 		}
-		if (!stack2.isEmpty()) {
-			return stack2.pop();
-		}
-	
-	    throw new IllegalStateException("队列为空");
-		
+		return stack2.pop();
 	}
-
 }
