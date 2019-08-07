f6fab56ccbe774949e92cf9ea12b56e6939d0537
iiHao <hg1111116@163.com>
2019-08-07 12:26:52 +0800

    offer18

---
+++ src/com/hao/offer/Offer18_01.java
@@ -0,0 +1,113 @@
+package com.hao.offer;
+
+import com.hao.common.ListNode;
+
+/**
+ * 【面试题18】 【删除单链表中节点】 给定单向链表的头指针和一个结点指针，定义一个函数在O(1)时间删除该结点。
+ * 
+ * @author hao
+ *
+ */
+public class Offer18_01 {
+	public static void main(String[] args) {
+		System.out.println("****功能测试***");
+		test1();
+		System.out.println("***仅有一个节点元素的单链表测试***");
+		test2();
+		System.out.println("***特殊输入测试****");
+		test3();
+	}
+
+	public static ListNode deleteNodeInList(ListNode pHead, ListNode pdelNode) {
+		return Solution1(pHead, pdelNode);
+	}
+
+	/**
+	 * 如果要删除的节点是中间节点，我们可以直接将它后面的一个节点的值赋给它， 而要删除的节点就变成了它后面的一个节点，
+	 * 要考虑删除节点是尾结点，和链表中只有一个节点的情况
+	 * 
+	 * @param pHead
+	 * @param pdelNode
+	 * @return
+	 */
+	private static ListNode Solution1(ListNode pHead, ListNode pdelNode) {
+		if (pHead == null || pdelNode == null) {
+			return pHead;
+		}
+		if (pdelNode.next != null) {// 要删除的节点在链表中间位置
+			ListNode p = pdelNode.next;
+			pdelNode.val = p.val;
+			pdelNode.next = p.next;
+			p = null;
+		} else if (pHead.next == pdelNode) {// 链表只有一个结点
+			pHead.next = pdelNode.next;
+			pdelNode = null;
+		} else {// 链表中有多的节点，要删除的节点是尾结点
+			ListNode p = pHead;
+			while (p.next != pdelNode) {// 搜寻pdelNode的前一个节点
+				p = p.next;
+			}
+			p.next = pdelNode.next;
+			pdelNode = null;
+		}
+		return pHead;
+	}
+
+	/**
+	 * 功能测试
+	 */
+	private static void test1() {//
+		System.out.println("(不加头结点)有三个节点元素的单链表");
+		ListNode node3 = new ListNode(9, null);
+		ListNode node2 = new ListNode(2, node3);
+		ListNode node1 = new ListNode(4, node2);
+		ListNode pHead = new ListNode(-1, node1);// 头结点
+		System.out.println("删除之前");
+		printListNode(pHead);
+		System.out.println("删除之后--->");
+		deleteNodeInList(pHead, node2);
+		printListNode(pHead);
+	}
+
+	private static void test2() {//
+		System.out.println("(不加头结点)仅有一个节点元素的单链表");
+		ListNode node1 = new ListNode(4, null);
+		ListNode pHead = new ListNode(-1, node1);// 头结点
+		System.out.println("删除之前");
+		printListNode(pHead);
+		System.out.println("删除之后-->");
+		deleteNodeInList(pHead, node1);
+		printListNode(pHead);
+	}
+
+	/**
+	 * 特殊输入测试
+	 */
+	private static void test3() {//
+		System.out.println("(不加头结点)有三个节点元素的单链表");
+		ListNode node3 = new ListNode(9, null);
+		ListNode node2 = new ListNode(2, node3);
+		ListNode node1 = new ListNode(4, node2);
+		ListNode pHead = new ListNode(-1, node1);// 头结点
+		System.out.println("删除之前");
+		printListNode(pHead);
+		System.out.println("删除之后(传入参数为null，null)--->");
+		deleteNodeInList(null, null);
+		printListNode(pHead);
+	}
+
+	/**
+	 * 打印链表
+	 * 
+	 * @param pHead
+	 */
+	private static void printListNode(ListNode pHead) {
+		ListNode p = pHead.next;
+		while (p != null) {
+			System.out.print(p.val + " ");
+			p = p.next;
+		}
+		System.out.println();
+	}
+
+}
---
+++ src/com/hao/offer/Offer18_02.java
@@ -0,0 +1,119 @@
+package com.hao.offer;
+
+import com.hao.common.ListNode;
+
+/**
+ * 【面试题18】 【删除链表中重复的节点】 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
+ * 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
+ * 
+ * @author hao
+ *
+ */
+public class Offer18_02 {
+
+	public static void main(String[] args) {
+		System.out.println("****功能测试***");
+		test1();
+		System.out.println("***特殊输入测试****");
+		test2();
+	}
+
+	public static ListNode deleteDuplication(ListNode pHead) {
+		return Solution1(pHead);
+	}
+
+	/**
+	 * 
+	 * 需要设置三个指针，pre pNode 和 next ;
+	 * 
+	 * @param pHead
+	 * @return
+	 */
+	private static ListNode Solution1(ListNode pHead) {
+		if (pHead == null || pHead.next == null) {// 如果链表为空或者只有一个节点
+			return pHead;
+		}
+		ListNode pre = null;
+		ListNode pNode = pHead; // 代表当前节点
+		while (pNode != null) {// 从头结点开始遍历链表
+			boolean needDel = false;
+			ListNode pNext = pNode.next;
+			if (pNext != null && pNext.val == pNode.val) {
+				// 如果当前节点的下一个结点不为null 且其值和当前节点相等
+				needDel = true;
+			}
+			if (!needDel) {// 如果没有找到重复的元素就将指针往后移动
+				pre = pNode;
+				pNode = pNode.next;
+			} else { // 找到重复的元素
+				ListNode toBeDel = pNode;
+				int value = toBeDel.val;
+				while (toBeDel != null && toBeDel.val == value) {
+					// 将指针移到所有重复元素的后面
+					toBeDel = toBeDel.next;
+				}
+				if (pre == null) {// 删除的是第一个节点
+					pHead = toBeDel;
+				} else {
+					pre.next = toBeDel;
+				}
+				pNode = toBeDel;
+			}
+		}
+
+		return pHead;
+	}
+
+	/**
+	 * 功能测试
+	 */
+	private static void test1() {//
+		System.out.println("有两对重复的");
+		ListNode node6 = new ListNode(6, null);
+		ListNode node5 = new ListNode(6, node6);
+		ListNode node4 = new ListNode(3, node5);
+		ListNode node3 = new ListNode(3, node4);
+		ListNode node2 = new ListNode(2, node3);
+		ListNode node1 = new ListNode(1, node2);
+		ListNode pHead = new ListNode(-1, node1);// 头结点
+		System.out.println("删除之前");
+		printListNode(pHead);
+		System.out.println("删除之后--->");
+		deleteDuplication(pHead);
+		printListNode(pHead);
+	}
+
+	/**
+	 * 
+	 */
+	private static void test2() {//
+		System.out.println("所有的元素都是重复的");
+		ListNode node6 = new ListNode(1, null);
+		ListNode node5 = new ListNode(1, node6);
+		ListNode node4 = new ListNode(1, node5);
+		ListNode node3 = new ListNode(1, node4);
+		ListNode node2 = new ListNode(1, node3);
+		ListNode node1 = new ListNode(1, node2);
+		ListNode pHead = new ListNode(-1, node1);// 头结点
+		System.out.println("删除之前");
+		printListNode(pHead);
+		System.out.println("删除之后--->");
+		deleteDuplication(pHead);
+		printListNode(pHead);
+	}
+
+	/**
+	 * 打印链表
+	 * 
+	 * @param pHead
+	 */
+	private static void printListNode(ListNode pHead) {
+		ListNode p = pHead.next;
+		while (p != null) {
+			System.out.print(p.val + " ");
+			p = p.next;
+		}
+		System.out.println();
+	}
+
+}
