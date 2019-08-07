76368520bfc932ae09fb4d1e8f0b08436a873742
iiHao <hg1111116@163.com>
2019-08-07 13:25:50 +0800

    18_02

--- src/com/hao/offer/Offer18_02.java
+++ src/com/hao/offer/Offer18_02.java
@@ -3,7 +3,7 @@ package com.hao.offer;
 import com.hao.common.ListNode;
 
 /**
- * 【面试题18】 【删除链表中重复的节点】 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
+ * 【面试题18】 【删除链表中重复的节点】 在一个排序的链表中，存在重复的结点， 请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
  * 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
  * 
  * @author hao
@@ -16,6 +16,8 @@ public class Offer18_02 {
 		test1();
 		System.out.println("***特殊输入测试****");
 		test2();
+		System.out.println("***特殊输入测试****");
+		test3();
 	}
 
 	public static ListNode deleteDuplication(ListNode pHead) {
@@ -30,12 +32,12 @@ public class Offer18_02 {
 	 * @return
 	 */
 	private static ListNode Solution1(ListNode pHead) {
-		if (pHead == null || pHead.next == null) {// 如果链表为空或者只有一个节点
+		if (pHead == null || pHead.next == null) {// 如果链表为空，或者已有一个节点
 			return pHead;
 		}
 		ListNode pre = null;
 		ListNode pNode = pHead; // 代表当前节点
-		while (pNode != null) {// 从头结点开始遍历链表
+		while (pNode != null) {// 从头指针开始遍历链表
 			boolean needDel = false;
 			ListNode pNext = pNode.next;
 			if (pNext != null && pNext.val == pNode.val) {
@@ -65,7 +67,7 @@ public class Offer18_02 {
 	}
 
 	/**
-	 * 功能测试
+	 * 功能测试 要注意此题目没有头结点
 	 */
 	private static void test1() {//
 		System.out.println("有两对重复的");
@@ -75,7 +77,7 @@ public class Offer18_02 {
 		ListNode node3 = new ListNode(3, node4);
 		ListNode node2 = new ListNode(2, node3);
 		ListNode node1 = new ListNode(1, node2);
-		ListNode pHead = new ListNode(-1, node1);// 头结点
+		ListNode pHead = new ListNode(0, node1);
 		System.out.println("删除之前");
 		printListNode(pHead);
 		System.out.println("删除之后--->");
@@ -84,17 +86,27 @@ public class Offer18_02 {
 	}
 
 	/**
-	 * 
+	 * 特殊值输入测试
 	 */
 	private static void test2() {//
 		System.out.println("所有的元素都是重复的");
-		ListNode node6 = new ListNode(1, null);
+		ListNode node6 = new ListNode(2, null);
 		ListNode node5 = new ListNode(1, node6);
 		ListNode node4 = new ListNode(1, node5);
 		ListNode node3 = new ListNode(1, node4);
 		ListNode node2 = new ListNode(1, node3);
 		ListNode node1 = new ListNode(1, node2);
-		ListNode pHead = new ListNode(-1, node1);// 头结点
+		ListNode pHead = new ListNode(1, node1);
+		System.out.println("删除之前");
+		printListNode(pHead);
+		System.out.println("删除之后--->");
+		deleteDuplication(pHead);
+		printListNode(pHead);
+	}
+
+	private static void test3() {
+		System.out.println("只有一个节点");
+		ListNode pHead = new ListNode(1, null);
 		System.out.println("删除之前");
 		printListNode(pHead);
 		System.out.println("删除之后--->");
@@ -103,17 +115,16 @@ public class Offer18_02 {
 	}
 
 	/**
-	 * 打印链表
+	 * 打印链表 此题目没有头结点
 	 * 
 	 * @param pHead
 	 */
 	private static void printListNode(ListNode pHead) {
-		ListNode p = pHead.next;
+		ListNode p = pHead;
 		while (p != null) {
 			System.out.print(p.val + " ");
 			p = p.next;
 		}
 		System.out.println();
 	}
-
 }
