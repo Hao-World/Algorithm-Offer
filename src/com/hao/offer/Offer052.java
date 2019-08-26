19f0f1358d1ff1005284e98a5c2d971ac5b48bb8
iiHao <hg1111116@163.com>
2019-08-26 11:49:52 +0800

    52

--- src/com/hao/offer/Offer052.java
+++ src/com/hao/offer/Offer052.java
@@ -21,9 +21,44 @@ public class Offer052 {
 	     return Solution1(pHead1,pHead2);
     }
 
+	/**
+	 * 利用长度关系
+	 * @param pHead1
+	 * @param pHead2
+	 * @return
+	 */
 	private static ListNode Solution1(ListNode pHead1, ListNode pHead2) {
+		int listLength1 = getListLength(pHead1);
+		int listLength2 = getListLength(pHead2);
+		int dif = listLength1-listLength2;
+		ListNode longList = pHead1;
+		ListNode shortList = pHead2;
+		if(listLength1<listLength2) {
+			longList = pHead2;
+			shortList = pHead1;
+			dif = listLength2-listLength1;
+		}
+		for(int i=0;i<dif;i++) {
+			longList = longList.next;
+		}
+		while(longList!=null && shortList!=null && longList!=shortList) {
+			longList = longList.next;
+			shortList = shortList.next;
+		}
+		ListNode firstCommonFirst = longList;
 		
-		return null;
+		return firstCommonFirst;
+	}
+	
+	
+
+	private static int getListLength(ListNode pHead1) {
+		int length = 0;
+		while(pHead1!=null) {
+			length++;
+			pHead1 = pHead1.next;
+		}
+		return length;
 	}
 
 	private static void test1() {
